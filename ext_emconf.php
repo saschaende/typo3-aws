bcd9bd79843bc572a4f8aabf417cb1f51e7f713b
Sascha Ende <sascha@sascha-ende.de>
2019-05-01 21:58:50 +0200

    Init 3.93.1

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Amazon AWS SDK',
+    'description' => 'Just install this extension and use amazon aws sdk.',
+    'category' => 'tools',
+    'author' => 'Filmmusic.io',
+    'author_email' => 'info@filmmusic.io',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '3.93.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.6.0-9.9.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
