def4f91352860155aadcead059465f2208a54f93
Sascha Ende <sascha@sascha-ende.de>
2019-05-03 11:24:13 +0200

    Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Amazon AWS SDK',
-    'description' => 'Just install this extension and use amazon aws sdk.',
+    'description' => 'The AWS SDK for PHP makes it easy for developers to access Amazon Web Services in their PHP code, and build robust applications and software using services like Amazon S3, Amazon DynamoDB, Amazon Glacier, etc.',
     'category' => 'tools',
     'author' => 'Filmmusic.io',
     'author_email' => 'info@filmmusic.io',
