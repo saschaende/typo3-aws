26b8a33520236b3b8f3bdc1cdf2e2996287e9561
Sascha Ende <sascha@sascha-ende.de>
2019-05-12 21:07:32 +0200

    Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Amazon AWS SDK',
-    'description' => 'The AWS SDK for PHP makes it easy for developers to access Amazon Web Services in their PHP code, and build robust applications and software using services like Amazon S3, Amazon DynamoDB, Amazon Glacier, etc.',
+    'description' => 'The AWS SDK for PHP makes it easy for developers to access Amazon Web Services in their PHP code, and build robust applications and software using services like Amazon S3, Amazon DynamoDB, Amazon Glacier, etc. Also install this extension via composer: composer require saschaende/typo3-aws',
     'category' => 'tools',
     'author' => 'Filmmusic.io',
     'author_email' => 'info@filmmusic.io',
