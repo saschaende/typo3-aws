468ea410b97fad81d4d367b1f3c1710bcde72693
Sascha Ende <sascha@sascha-ende.de>
2020-11-06 11:23:59 +0100

    Update to 3.67

--- Resources/Private/Libraries/vendor/composer/autoload_real.php
+++ Resources/Private/Libraries/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit2624a00c84be2759ccc79422ba9e19a9
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
